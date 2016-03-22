4930752699c7fbe817ad950246dd7eb469b75f73
Leandro Consoli <lgconsoli@univel.br>
2016-03-22 15:17:47 -0300

    Alteração solicitada pelo cliente.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[]args){
 		String msg = "Hello World!";
+		System.out.println("----------------");
 		System.out.println(msg);
+		System.out.println("----------------");
 	}
 	
 }
