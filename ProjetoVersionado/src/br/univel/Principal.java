66b018eb88c45c672dc689e2bd4c207901675b61
Leandro Consoli <lgconsoli@univel.br>
2016-03-22 15:09:59 -0300

    Terceira versão.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,7 +3,7 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[]args){
-		String msg = "olá mundo!";
+		String msg = "Hello World!";
 		System.out.println(msg);
 	}
 	
