3caae305166bc1f8156ff6ddeda729092f5921e5
Leandro Consoli <lgconsoli@univel.br>
2016-03-22 15:37:35 -0300

    Nova correção.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,11 +3,10 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[]args){
-		String hello = "Hello";
-		String world = "World";
+		String hello = "Hello ";
+		String world = "World ";
 		String msg = hello + world + "!";
 		System.out.println(msg);
-		System.out.println("teste");
 	}
 	
 }
